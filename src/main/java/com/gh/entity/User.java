e9a42f6bbc72f289d591a56ae48456e5c40ea3c0
zhanghai1002 <m13638657289@163.com>
2019-11-26 17:34:51 +0800

    mybatis-demo

---
+++ src/main/java/com/gh/entity/User.java
@@ -0,0 +1,14 @@
+package com.gh.entity;
+
+import lombok.Data;
+
+/**
+ * @Author zhanghai
+ * @create 2019/11/26 17:28
+ */
+@Data
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+}
